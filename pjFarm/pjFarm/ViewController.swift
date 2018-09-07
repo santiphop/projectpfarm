8ced976e347476b15d51a67e7de3ea64fc2570f0
pawitra phu <pawitra.p@ku.th>
2018-09-07 16:41:35 +0700

    test commit1

---
+++ pjFarm/pjFarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pjFarm
+//
+//  Created by iOS Dev on 9/7/2561 BE.
+//  Copyright © 2561 iOS Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pjFarm/pjFarmTests/pjFarmTests.swift
@@ -0,0 +1,36 @@
+//
+//  pjFarmTests.swift
+//  pjFarmTests
+//
+//  Created by iOS Dev on 9/7/2561 BE.
+//  Copyright © 2561 iOS Dev. All rights reserved.
+//
+
+import XCTest
+@testable import pjFarm
+
+class pjFarmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
